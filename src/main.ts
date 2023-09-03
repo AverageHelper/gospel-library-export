9111f59bd471ae9d1f1c99b401f0835fc184c37b
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2023-09-02 20:45:01 -0600

    fix: Show an informative message if the login cookie has expired

--- src/main.ts
+++ src/main.ts
@@ -46,19 +46,27 @@ let previousCookie: string | null = null;
 async function requestCookie(fresh: boolean): Promise<string> {
 	if (!fresh && previousCookie) return previousCookie;
 
-	console.info("-- Gospel Library Notes Inspector --");
-	console.info("We need your login token in order to access your notes.");
-	console.info(
-		"Because I don't want to bother setting up OAuth to talk to churchofjesuschrist.org the 'proper' way, I'll ask you for your login cookie directly."
-	);
-	console.info("\t1. Log in to https://churchofjesuschrist.org");
-	console.info("\t2. Open browser devtools");
-	console.info("\t3. Open Network inspector");
-	console.info("\t4. Go to Notes");
-	console.info('\t5. Look for one of the requests to an endpoint that starts with "v3"');
-	console.info("\t6. Find the Cookie header sent in that request");
-	console.info('\t7. Right click + "Copy Value"');
-	console.info("\t8. Paste the value here:");
+	if (previousCookie) {
+		// We had a cookie, but we need a new one
+		console.info(
+			"Your login cookie has expired. Please reload your Notes window and copy a fresh 'Cookie' header to paste here:"
+		);
+	} else {
+		// We didn't have a cookie before
+		console.info("-- Gospel Library Notes Inspector --");
+		console.info("We need your login token in order to access your notes.");
+		console.info(
+			"Because I don't want to bother setting up OAuth to talk to churchofjesuschrist.org the 'proper' way, I'll ask you for your login cookie directly."
+		);
+		console.info("\t1. Log in to https://churchofjesuschrist.org");
+		console.info("\t2. Open browser devtools");
+		console.info("\t3. Open Network inspector");
+		console.info("\t4. Go to Notes");
+		console.info('\t5. Look for one of the requests to an endpoint that starts with "v3"');
+		console.info("\t6. Find the 'Cookie' header sent in that request");
+		console.info('\t7. Right click + "Copy Value"');
+		console.info("\t8. Paste the value here:");
+	}
 
 	const { Cookie } = await inquirer.prompt<{ Cookie: string }>({
 		type: "password",
