9142e5da8ff94c066b1a23d9f41307590dfd73cd
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2023-09-15 15:43:53 -0600

    chore: Add a comment about `jest-extended` in `bun test`

--- vitestSetup.ts
+++ vitestSetup.ts
@@ -1,3 +1,3 @@
 import { expect } from "vitest";
 import * as matchers from "jest-extended";
-expect.extend(matchers);
+expect.extend(matchers); // Won't work in bun until https://github.com/oven-sh/bun/issues/3621
