c3bfdd41ebe7cf8dd826ae8cb09b839fff267a25
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2023-09-15 15:34:26 -0600

    fix: Improved types for filesystem proxy

--- src/helpers/fs.ts
+++ src/helpers/fs.ts
@@ -1,8 +1,11 @@
-import type { Dirent } from "node:fs";
+import type { Dirent, PathLike } from "node:fs";
 import { mkdir, readdir, readFile, writeFile } from "node:fs/promises";
 
 export const fs = {
-	mkdir,
+	async mkdir(this: void, path: PathLike): Promise<void> {
+		return await mkdir(path);
+	},
+
 	async readdir(
 		this: void,
 		path: PathLike,
@@ -14,10 +17,19 @@ export const fs = {
 	): Promise<Array<Dirent>> {
 		return await readdir(path, options);
 	},
-	async readFile(this: void, path: PathOrFileDescriptor, options: BufferEncoding): Promise<string> {
+
+	async readFile(this: void, path: PathLike, options: BufferEncoding): Promise<string> {
 		return await readFile(path, options);
 	},
-	writeFile
+
+	async writeFile(
+		this: void,
+		path: PathLike,
+		data: string,
+		options: { encoding?: BufferEncoding | null | undefined }
+	) {
+		return await writeFile(path, data, options);
+	}
 } as const;
 
 export type FilesystemProxy = typeof fs;
