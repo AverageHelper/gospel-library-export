24bd602d2a9c3b80403f5167933bde5f0fa91d25
AverageHelper <me@average.name>
2023-11-25 16:10:11 -0700

    ci: Start moving to Codeberg

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,49 +1,36 @@
 # Changelog
-
 All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [1.1.3] - 2023-09-17
-
 ### Changed
-
 - Simplifed internal UI calls.
 - Scrolling list views now wrap around. To get quickly to the bottom of a long list, just go up!
 
 ## [1.1.2] - 2023-09-15
-
 ### Changed
-
 - Reorganized filesystem APIs so they're easier to mock on different test platforms.
 
 ## [1.1.1] - 2023-09-11
-
 ### Fixed
-
 - Restored `-v` command line argument to print the app's version.
 
 ## [1.1.0] - 2023-09-07
-
 ### Added
-
 - Option to download all annotations.
 - Option to read annotations from a local file instead of online.
 
 ### Changed
-
 - Simplified login instructions slightly.
 
 ### Fixed
-
 - Handle `purple` and `dark_blue` mark colors.
 - Don't require a login cookie to download document contents. As far as I know, they're all accessible by anyone now.
 
 ## [1.0.0] - 2023-09-04
-
 ### Added
-
 - Load and view notes and annotations from https://www.churchofjesuschrist.org.
 - Navigation in CLI by arrow keys.
 
--- scripts/release.ts
+++ scripts/release.ts
@@ -1,18 +1,16 @@
 #!/usr/bin/env ts-node
 
 import "./assertTsNode.js";
-import { assert, literal, string, type } from "superstruct";
+import { assert, enums, string, type } from "superstruct";
 import { parser as changelogParser } from "keep-a-changelog";
 import { readFileSync, writeFileSync } from "node:fs";
 import { URL } from "node:url";
-import * as semver from "semver";
+import { parse as parseSemVer } from "semver";
 
 const logger = console;
 
 // Fixes the changelog's footer links and bumps the `version` in [package.json](/package.json) and [package-lock.json](/package-lock.json).
 // This script may be run repeatedly on the same project.
-
-const { parse: parseSemVer } = semver;
 
 function quote(str: string | undefined): string | undefined {
 	if (str === undefined) return str;
@@ -22,10 +20,9 @@ function quote(str: string | undefined): string | undefined {
 logger.info("** release.ts **");
 
 // Load the changelog
-const here = import.meta.url;
-const changelogPath = new URL("../CHANGELOG.md", here).pathname;
-const packageJsonPath = new URL("../package.json", here).pathname;
-const packageLockJsonPath = new URL("../package-lock.json", here).pathname;
+const changelogPath = new URL("../CHANGELOG.md", import.meta.url).pathname;
+const packageJsonPath = new URL("../package.json", import.meta.url).pathname;
+const packageLockJsonPath = new URL("../package-lock.json", import.meta.url).pathname;
 logger.info("Loading changelog from", quote(changelogPath));
 
 const rawChangelog = readFileSync(changelogPath, "utf-8");
@@ -34,20 +31,20 @@ const changelog = changelogParser(rawChangelog);
 const releases = changelog.releases;
 
 // Get current versioned release
-const thisReleaseIdx = releases.findIndex(release => release.date && release.version);
+const thisReleaseIdx = releases.findIndex(release => release.date && release.parsedVersion);
 const thisRelease = releases[thisReleaseIdx];
-if (!thisRelease?.version) throw new TypeError("No versioned release was found.");
+if (!thisRelease?.parsedVersion || !thisRelease.version)
+	throw new TypeError("No versioned release was found.");
 
 // Handy info
-logger.info("latest release:", thisRelease.version?.toString());
+logger.info("latest release:", thisRelease.version);
 
 const prevRelease = releases[thisReleaseIdx + 1];
-logger.info("previous release:", prevRelease?.version?.toString());
+logger.info("previous release:", prevRelease?.version);
 
 // Fix the changelog's format (new compare links, etc.), and print the diff of our changes
 logger.info("\n** Spec compliance **");
 
-changelog.format = "markdownlint";
 const newChangelog = changelog.toString();
 writeFileSync(changelogPath, newChangelog);
 
@@ -65,7 +62,7 @@ const versioned = type({
 });
 const versionedLock = type({
 	version: string(),
-	lockfileVersion: literal(3),
+	lockfileVersion: enums([2, 3]),
 	packages: type({
 		"": type({
 			version: string()
@@ -82,30 +79,27 @@ assert(packageLockJson, versionedLock);
 const packageVersion = parseSemVer(packageJson.version);
 const packageLockVersion = parseSemVer(packageLockJson.version);
 
-if (!packageVersion) {
+if (!packageVersion)
 	throw new TypeError(
 		'The "version" field in package.json is not a compliant Semantic Version number'
 	);
-}
 
-if (!packageLockVersion) {
+if (!packageLockVersion)
 	throw new TypeError(
 		'The "version" field in package-lock.json is not a compliant Semantic Version number'
 	);
-}
 
 logger.info("package.json version:", packageVersion.version);
 logger.info("package-lock.json version:", packageLockVersion.version);
 
-if (packageVersion.version !== packageLockVersion.version) {
+if (packageVersion.version !== packageLockVersion.version)
 	throw new EvalError(
 		'The "version" fields in package.json and package-lock.json do not match. Please let CHANGELOG.md be the source of truth for versioning. To ignore this warning and proceed, please first run `npm install`.'
 	);
-}
 
 // Update package.json
 const oldPackageJson = `${JSON.stringify(packageJson, undefined, "\t")}\n`;
-packageJson.version = thisRelease.version.version;
+packageJson.version = thisRelease.version;
 const newPackageJson = `${JSON.stringify(packageJson, undefined, "\t")}\n`;
 writeFileSync(packageJsonPath, newPackageJson);
 
@@ -119,8 +113,8 @@ if (!didFixPackageJson) {
 // Update package-lock.json
 const oldPackageLockJson = `${JSON.stringify(packageLockJson, undefined, "\t")}\n`;
 // Maybe we should just run `npm i` instead?
-packageLockJson.version = thisRelease.version.version;
-packageLockJson.packages[""].version = thisRelease.version.version;
+packageLockJson.version = thisRelease.version;
+packageLockJson.packages[""].version = thisRelease.version;
 const newPackageLockJson = `${JSON.stringify(packageLockJson, undefined, "\t")}\n`;
 writeFileSync(packageLockJsonPath, newPackageLockJson);
 
